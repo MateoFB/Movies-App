5cdcf6fcfc0d248f4741b2ce82d2583ee9436dd8
MateoFernandezBanchi <matefbanchi@gmail.com>
2025-08-08 10:23:44 -0500

    api connection

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,14 @@ import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import "../globals.css";
 
 export default function Index() {
@@ -18,6 +25,61 @@ export default function Index() {
 
   console.log(movies);
 
+  let content;
+  if (moviesLoading) {
+    content = (
+      <ActivityIndicator
+        size="large"
+        color="#fff"
+        className="mt-10 justify-self-center"
+      />
+    );
+  } else if (moviesError) {
+    content = (
+      <Text className="text-white text-lg mt-10">Error: {moviesError}</Text>
+    );
+  } else {
+    content = (
+      <View className="flex-1 mt-5 ">
+        <SearchBar
+          onPress={() => router.push("/search")}
+          placeHolder="Buscar"
+        />
+        <Text className="text-white font-bold text-lg mt-10 mb-3">
+          Películas Populares
+        </Text>
+        <FlatList
+          data={movies}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={3}
+          columnWrapperStyle={{ justifyContent: "flex-start", gap:20, paddingBottom: 10, paddingRight: 5 }}
+          className="mt-2 pb-32"
+          scrollEnabled={false}
+          renderItem={({ item }) => (
+            <View className="">
+              <Image
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                }}
+                className="w-32 h-48 rounded-lg"
+              />
+              <Text className="text-white font-semibold mt-2">
+                {item.title}
+              </Text>
+            </View>
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+          ListEmptyComponent={
+            <Text className="text-white text-lg text-center mt-10">
+              No se encontraron películas.
+            </Text>
+          }
+        />
+      </View>
+    );
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-primary">
       <Image className="w-full z-0 absolute mt-0 top-0" source={images.bg} />7
@@ -27,12 +89,7 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5 ">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeHolder="Buscar"
-          />
-        </View>
+        {content}
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -1,16 +1,16 @@
 export const TMBD_API_CONFIG = {
-  BASE_URL: "https://api.themoviedb.org/3/",
-  API_KEY: process.env.TOKEN,
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_API_KEY,
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.TOKEN}`,
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_API_KEY}`,
   },
 };
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `${TMBD_API_CONFIG.BASE_URL}{search/movie?query=${encodeURIComponent(query)}}`
-    : `${TMBD_API_CONFIG.BASE_URL}{discover/movie?sort_by=popularity.desc}`;
+    ? `${TMBD_API_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMBD_API_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
   const response = await fetch(endpoint, {
     method: "GET",
     headers: TMBD_API_CONFIG.headers,
@@ -21,21 +21,5 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   }
 
   const data = await response.json();
-  return data;
+  return data.results || data; // Return results if available, otherwise return the full data
 };
-
-// const url =
-//   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-// const options = {
-//   method: "GET",
-//   headers: {
-//     accept: "application/json",
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Mjg0Mjg2NGE4NGUyMTllZTFmNzQ2Mjg3N2FkMjIwNyIsIm5iZiI6MTYyMzg2ODg2Ni45OTIsInN1YiI6IjYwY2E0NWMyOTkyNTljMDA3OTIzMzgzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v8GvrhGz6BK33QmudQMSAp0mNxAlY1YrNUfJDV-5xuw",
-//   },
-// };
-
-// fetch(url, options)
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-//   .catch((err) => console.error(err));
